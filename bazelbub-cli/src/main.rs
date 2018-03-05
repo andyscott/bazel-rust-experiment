a8e9012a2bece9802526fc009bdbe22cc100a325
Andy Scott <andy.g.scott@gmail.com>
2018-03-05 13:49:37 -0800

    Flush out console command so it is actually usable

--- bazelbub-cli/src/main.rs
+++ bazelbub-cli/src/main.rs
@@ -78,13 +78,21 @@ fn command_console(target: &str) -> () {
         &format!("--override_repository=bazelbub={}", dir.display());
     let flag_output_groups = "--output_groups=bazelbub";
 
-    let mut vec: Vec<&str> = Vec::new();
-    vec.push(flag_aspects);
-    vec.push(flag_override_repository);
-    vec.push(flag_output_groups);
-    vec.push(target);
+    let mut vec1: Vec<&str> = Vec::new();
+    vec1.push(flag_aspects);
+    vec1.push(flag_override_repository);
+    vec1.push(flag_output_groups);
+    vec1.push(target);
 
-    call_bazel("build", vec);
+    call_bazel("build", vec1);
+
+    let mut vec2: Vec<&str> = Vec::new();
+    vec2.push(flag_aspects);
+    vec2.push(flag_override_repository);
+    vec2.push(flag_output_groups);
+    vec2.push("@scala//:scala-compiler");
+
+    call_bazel("build", vec2);
 
     drop(file_workspace);
     drop(file_build);
@@ -103,20 +111,40 @@ fn command_console(target: &str) -> () {
     let bits = sout2.trim().split(':').collect::<Vec<&str>>();
     let (package, name) = (bits[0], bits[1]);
 
-    let output_path = &format!("{}{}/bazelbub.{}.json", bazel_bin, package, name);
+    let output3 =
+        Command::new("bazel").args(&["info", "execution_root"])
+        .output().unwrap();
+    let sout3 = String::from_utf8(output3.stdout).unwrap();
+    let execution_root = sout3.trim();
 
-    println!("foo: {}", output_path);
+    let output_path1 = &format!("{}{}/bazelbub.{}.json", bazel_bin, package, name);
+    let output_path2 = &format!("{}/external/scala/bazelbub.scala-compiler.json", bazel_bin);
 
-    let mut data = String::new();
-    let mut f = File::open(output_path).unwrap();
-    f.read_to_string(&mut data).unwrap();
+    let mut data1 = String::new();
+    let mut file1 = File::open(output_path1).unwrap();
+    file1.read_to_string(&mut data1).unwrap();
 
-    let aspect_info: AspectInfo = serde_json::from_str(&data).ok().unwrap();
+    let mut data2 = String::new();
+    let mut file2 = File::open(output_path2).unwrap();
+    file2.read_to_string(&mut data2).unwrap();
 
-    println!("launch ammonite with classpath:");
-    for entry in &aspect_info.runtime_classpath {
-        println!(" - {}", entry);
-    }
+    let aspect_info1: AspectInfo = serde_json::from_str(&data1).ok().unwrap();
+    let aspect_info2: AspectInfo = serde_json::from_str(&data2).ok().unwrap();
+
+    let full_classpath =
+        aspect_info1.runtime_classpath.iter().chain(
+            aspect_info2.runtime_classpath.iter())
+        .map(|p| format!("{}/{}", execution_root, p))
+        .collect::<Vec<String>>();
+
+    Command::new("java")
+        .args(&["-cp", &full_classpath.join(":")])
+        .arg("scala.tools.nsc.MainGenericRunner")
+        .arg("-usejavacp")
+        .spawn()
+        .unwrap_or_else(|e| { panic!("failed to run java: {}", e) })
+        .wait()
+        .unwrap();
 }
 
 fn call_bazel(cmd: &str, args: Vec<&str>) -> () {
